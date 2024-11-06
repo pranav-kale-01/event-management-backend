c9a676cc3909da650f5319cf08057156300c63c6
Pranav Kale <pranavkale021998@gmail.com>
2024-11-06 13:18:38 +0530

    Updated Vercel confguration for deployment

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const connectDB = require('./config/db');
-const authRoutes = require('./routes/auth');
-const eventRoutes = require('./routes/events');
-const chatRoutes = require('./routes/chat');
-const protectedRoutes = require('./routes/protected');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth");
+const eventRoutes = require("./routes/events");
+const chatRoutes = require("./routes/chat");
+const protectedRoutes = require("./routes/protected");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,14 +14,18 @@ const PORT = process.env.PORT || 3001;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/events', eventRoutes);
-app.use('/api', protectedRoutes);
-app.use('/api/chats', chatRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventRoutes);
+app.use("/api", protectedRoutes);
+app.use("/api/chats", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
